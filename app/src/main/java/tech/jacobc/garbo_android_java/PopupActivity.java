651f49eba27b79064619b3371e23ff7f22717666
Johnathan Bevers <beversjohnathan7@gmail.com>
2021-10-31 18:49:38 -0500

    Added Select Image Ability

--- app/src/main/java/tech/jacobc/garbo_android_java/PopupActivity.java
+++ app/src/main/java/tech/jacobc/garbo_android_java/PopupActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class PopupActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +17,17 @@ public class PopupActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(PopupActivity.this, MapsActivity.class));
+            }
+        });
+        Button imageButton = findViewById(R.id.button3);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PopupActivity.this, MapsActivity.class));
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
             }
         });
     }
